479ca89d1b1b567e4f31395f29901d258354d761
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-03-29 19:09:52 +0000

    Added code to app.py to connect the os to the app in models and templates, base, etc

---
+++ app.py
@@ -0,0 +1,7 @@
+import os
+from spanresources import app
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", 5000)),
+        debug=os.environ.get("DEBUG", False))
